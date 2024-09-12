f92a47289cc53191869f64ccf81cdf0b25d87bff
ftovar8 <felix.godo@gmail.com>
2024-09-12 17:30:25 -0300

    initial version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-module.exports = new class {
-    check(port) {
-        return new Promise((resolve) => {
-            const tester = require('net').createServer()
-                .once('error', () => resolve(false))
-                .once('listening', () => tester.once('close', () => resolve(true)).close())
-                .listen(port);
-        });
-    }
-}
+module.exports = new (class {
+	check(port) {
+		return new Promise(resolve => {
+			const tester = require('net')
+				.createServer()
+				.once('error', () => resolve(false))
+				.once('listening', () => tester.once('close', () => resolve(true)).close())
+				.listen(port);
+		});
+	}
+})();
